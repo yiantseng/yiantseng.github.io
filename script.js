7975cf5b9b49198c8bb58c9f0c5ab9f7647c683d
Yi-An Tseng <yiantseng@gmail.com>
2014-03-28 13:47:13 -0700

    Initial Design
    Initial Design

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('#github').mouseover(function(){
+        $('#github').animate({opacity:1});
+    });
+
+    $('#github').mouseout(function(){
+        $('#github').animate({opacity:0.60});
+    });
+
+     $('#linkedin').mouseenter(function(){
+        $('#linkedin').animate({opacity:1});
+    });
+
+    $('#linkedin').mouseout(function(){
+        $('#linkedin').animate({opacity:0.60});
+    });
+
+     $('#gmail').mouseenter(function(){
+        $('#gmail').animate({opacity:1});
+    });
+
+    $('#gmail').mouseout(function(){
+        $('#gmail').animate({opacity:0.60});
+    });
+
+});
